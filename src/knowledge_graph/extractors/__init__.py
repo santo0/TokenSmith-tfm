5580d1d2db9e14f6f07f8b2243564236ab1963a5
santo0 <lemarti3472@gmail.com>
2026-04-16 16:31:11 -0400

    feat: keyword extraction experiment

--- src/knowledge_graph/extractors/__init__.py
+++ src/knowledge_graph/extractors/__init__.py
@@ -1,8 +1,11 @@
 from src.knowledge_graph.extractors.base_extractor import BaseExtractor
 from src.knowledge_graph.extractors.slm_extractor import SLMExtractor
 from src.knowledge_graph.extractors.keybert_extractor import KeyBERTExtractor
-from src.knowledge_graph.extractors.openrouter_extractor import OpenRouterExtractor
+from src.knowledge_graph.extractors.openrouter_extractor import (
+    OpenRouterExtractor,
+)
 from src.knowledge_graph.extractors.json_extractor import JsonExtractor
+from src.knowledge_graph.extractors.yake_extractor import YakeExtractor
 
 __all__ = [
     "BaseExtractor",
@@ -10,4 +13,5 @@ __all__ = [
     "KeyBERTExtractor",
     "OpenRouterExtractor",
     "JsonExtractor",
+    "YakeExtractor",
 ]
